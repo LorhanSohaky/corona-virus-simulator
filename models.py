62d93c10b1e4195140f26934da7bb7b74e186bd1
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-08 11:29:05 -0300

    Refactore model

--- models.py
+++ models.py
@@ -1,12 +1,5 @@
+from mesa import Model
 from mesa_geo import GeoSpace, GeoAgent, AgentCreator
-from mesa import Agent, Model
-from mesa.time import RandomActivation
-from mesa.datacollection import DataCollector
-from mesa.space import MultiGrid
-import requests
-url = 'http://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_20m.json'
-r = requests.get(url)
-geojson_states = r.json()
 
 class State(GeoAgent):
     def __init__(self, unique_id, model, shape):
@@ -18,55 +11,5 @@ class GeoModel(Model):
         
         state_agent_kwargs = dict(model=self)
         AC = AgentCreator(agent_class=State, agent_kwargs=state_agent_kwargs)
-        agents = AC.from_GeoJSON(GeoJSON=geojson_states, unique_id="NAME")
+        agents = AC.from_file('custom.geo.json',unique_id="labelrank")
         self.grid.add_agents(agents)
-
-
-# class HumanAgent(Agent):
-#     def __init__(self, unique_id, model, infected):
-#         super().__init__(unique_id, model)
-#         self.infected = infected
-
-#     def move(self):
-#         possible_steps = self.model.grid.get_neighborhood(
-#             self.pos,
-#             moore=True,
-#             include_center=False)
-#         new_position = self.random.choice(possible_steps)
-#         self.model.grid.move_agent(self, new_position)
-
-#     def infect(self):
-#         cellmates = self.model.grid.get_cell_list_contents([self.pos])
-#         if len(cellmates) > 1:
-#             other = self.random.choice(cellmates)
-#             other.infected = True
-
-#     def step(self):
-#         self.move()
-#         if self.infected:
-#             self.infect()
-
-# class HumanModel(Model):
-#     def __init__(self, N, width, height):
-#         self.num_agents = N
-#         self.grid = MultiGrid(width, height, True)
-#         self.schedule = RandomActivation(self)
-#         self.running = True
-
-#         # Create a infected agent
-#         infectedHuman = HumanAgent(0, self, True)
-#         self.schedule.add(infectedHuman)
-#         x = 3
-#         y = 3
-#         self.grid.place_agent(infectedHuman, (x, y))
-
-#         for i in range(1,self.num_agents):
-#             a = HumanAgent(i, self, False)
-#             self.schedule.add(a)
-#             # Add the agent to a random grid cell
-#             x = self.random.randrange(self.grid.width)
-#             y = self.random.randrange(self.grid.height)
-#             self.grid.place_agent(a, (x, y))
-
-    def step(self):
-        self.schedule.step()
--- run.py
+++ run.py
@@ -1,52 +1,8 @@
-
-from mesa.visualization.modules import CanvasGrid
-from mesa.visualization.modules import ChartModule
 from mesa_geo.visualization.ModularVisualization import ModularServer
 from mesa_geo.visualization.MapModule import MapModule
 
-# def agent_portrayal(agent):
-#     portrayal = {
-#       "Shape": "circle",
-#       "Color": "red" if agent.infected else "blue",
-#       "Filled": "true",
-#       "Layer": 0,
-#       "infected":agent.infected,
-#       "r": 0.5
-#     }
+from models import GeoModel
 
-#     return portrayal
-
-# grid = CanvasGrid(agent_portrayal, 5, 5, 500, 500)
-
-# server = ModularServer(HumanModel,
-#                        [grid],
-#                        "Human Model",
-#                        {"N":3, "width":5, "height":5})
-
-# server.port = 8521 # The default
-# server.launch()
-
-from mesa_geo import GeoSpace, GeoAgent, AgentCreator
-from mesa import Model
-
-class State(GeoAgent):
-    def __init__(self, unique_id, model, shape):
-        super().__init__(unique_id, model, shape)
-
-class GeoModel(Model):
-    def __init__(self):
-        self.grid = GeoSpace()
-        
-        state_agent_kwargs = dict(model=self)
-        AC = AgentCreator(agent_class=State, agent_kwargs=state_agent_kwargs)
-        agent = AC.create_agent([26.11,43.97],1)
-        ##agents = AC.from_file('countries-land-10km.geo.json',unique_id="A3")
-        self.grid.add_agents(agent)
-
-m = GeoModel()
-
-agent = m.grid.agents[0]
-print(agent.unique_id)
 
 def schelling_draw(agent):
     """
@@ -62,7 +18,7 @@ import profile
 map_element = MapModule(schelling_draw, [52, 12], 4, 500, 500)
 server = ModularServer(GeoModel,
                        [map_element],
-                       "Human Model")
+                       "Coronavirus simulator")
 
 server.port = 8521 # The default
 server.launch()
